e60bb3b79da31b6df26da1470317aea34fb96d81
nikikalwar <nikikumar27@gmail.com>
2020-10-11 07:25:27 +0530

    a failed example

--- getting_started/matchers/app.test.js
+++ getting_started/matchers/app.test.js
@@ -21,7 +21,7 @@ this checks for exact equality
     }
   });
   */
-
+/*
  test('null', () => {
     const n = null;
     expect(n).toBeNull();
@@ -38,4 +38,17 @@ this checks for exact equality
     expect(z).not.toBeUndefined();
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
+  });
+*/
+
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(9);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+  
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
   });
